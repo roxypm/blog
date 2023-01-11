5d9ea295c67acc0b2799c91fd0f6a3256d949608
aniyadyx <930505227@qq.com>
2023-01-11 23:25:09 +0800

    up

--- source/js/custom.js
+++ source/js/custom.js
@@ -3,14 +3,14 @@ const topImgEl = document.querySelector(topImgElID);
 console.log(topImgEl);
 if (topImgEl) {
   const imgArr = [
-    "/images/bizhi/0f2399283302014827121ee771587341.jpg",
-    "/images/bizhi/1c14d5734016d8bfea115531f94b0227.jpg",
-    "/images/bizhi/2d95699cd55814cbc2b092a4b72ba172.jpg",
-    "/images/bizhi/3a5e160d8218fea55e57f71f304d50da (1).jpg",
-    "/images/bizhi/3b83a7ee10f044dabfe9d52c7e8e15f1.jpg",
-    "/images/bizhi/5.png",
-    "/images/bizhi/3a5e160d8218fea55e57f71f304d50da.jpg",
-    "/images/bizhi/e94a2c60934bd7aeb4a748f0cb6a2e6f.jpg",
+    "https://alyx111.oss-cn-shenzhen.aliyuncs.com/bizhi/0f2399283302014827121ee771587341.jpg",
+    "https://alyx111.oss-cn-shenzhen.aliyuncs.com/bizhi/1c14d5734016d8bfea115531f94b0227.jpg",
+    "https://alyx111.oss-cn-shenzhen.aliyuncs.com/bizhi/2d95699cd55814cbc2b092a4b72ba172.jpg",
+    "https://alyx111.oss-cn-shenzhen.aliyuncs.com/bizhi/3a5e160d8218fea55e57f71f304d50da (1).jpg",
+    "https://alyx111.oss-cn-shenzhen.aliyuncs.com/bizhi/3b83a7ee10f044dabfe9d52c7e8e15f1.jpg",
+    "https://alyx111.oss-cn-shenzhen.aliyuncs.com/bizhi/5.png",
+    "https://alyx111.oss-cn-shenzhen.aliyuncs.com/bizhi/3a5e160d8218fea55e57f71f304d50da.jpg",
+    "https://alyx111.oss-cn-shenzhen.aliyuncs.com/bizhi/e94a2c60934bd7aeb4a748f0cb6a2e6f.jpg",
    
   ];
   const imgRandomIndex = Math.floor(Math.random() * imgArr.length);
